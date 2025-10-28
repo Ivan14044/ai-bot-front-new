3b46c4a919e649ca99112f0f1e9e5415fd7e705f
Ivan14044 <knysh_ivan@ukr.net>
2025-10-28 09:12:32 +0200

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src')
         }
     },
+    base: '/ai-bot-front-new/',
     build: {
         outDir: 'dist',
         sourcemap: false
