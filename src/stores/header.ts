27d3f62606eea632be5b54f8315de94b713811b8
Ivan14044 <knysh_ivan@ukr.net>
2025-10-28 08:56:55 +0200

    remove all files

--- src/stores/header.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia';
-
-export const useHeaderStore = defineStore('header', {
-    state: () => ({
-        isReady: false,
-        showMenu: false,
-        printedText: ''
-    })
-});
